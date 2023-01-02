0a6d1089adc229af0b0e247636ea49f75c64f2b8
alexisvafiadis <104735242+alexisvafiadis@users.noreply.github.com>
2023-01-02 15:04:02 +0100

    Update betterlabor.sql

--- database/betterlabor.sql
+++ database/betterlabor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2022 at 05:19 PM
+-- Generation Time: Jan 02, 2023 at 03:02 PM
 -- Server version: 5.7.17
 -- PHP Version: 7.1.3
 
@@ -25,6 +25,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ID` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activated` tinyint(4) DEFAULT '0',
+  `code_verification` varchar(50) NOT NULL,
+  `activation_expiry` datetime NOT NULL,
+  `statut` tinyint(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`ID`, `email`, `password`, `prenom`, `nom`, `created_at`, `updated_at`, `activated`, `code_verification`, `activation_expiry`, `statut`) VALUES
+(8, 'alexisvafiadis@gmail.com', '$2y$10$Y9YGTyhx23ECWzY2QuIccuSiju.vI70IrEqa5vbJ2f3mFAihKjlrC', 'Alexis', 'Vafiadis', '2022-12-16 14:15:29', '2022-12-16 14:15:29', 0, 'b1352f0cb8ae9e46', '2022-12-17 14:15:29', NULL),
+(7, 'flazebrawl@gmail.com', '$2y$10$cbxaSgjdu.DqKyCyBGphpuzlE0g93tfBIU/O.ph6fJ7zX60/DVWGm', 'Alexis', 'Vafiadis', '2022-12-16 09:29:37', '2022-12-16 09:29:37', 0, 'dae835d1300ae17e', '2022-12-17 09:29:37', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `capteur`
 --
 
@@ -41,27 +69,63 @@ CREATE TABLE `capteur` (
 --
 
 CREATE TABLE `categorie` (
-  `Nom` varchar(100) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `nom` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `position` tinyint(4) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categorie`
+--
+
+INSERT INTO `categorie` (`ID`, `nom`, `position`) VALUES
+(1, 'Questions generales!', 1),
+(2, 'Question pour chef de chantier', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `entreprise`
+-- Table structure for table `chef`
 --
 
-CREATE TABLE `entreprise` (
-  `ID` bigint(20) NOT NULL,
-  `ID_manager` bigint(20) NOT NULL,
-  `nom` varchar(50) NOT NULL,
-  `activite` tinyint(4) NOT NULL,
-  `nombre_slots` mediumint(9) NOT NULL,
-  `adresse` varchar(120) NOT NULL,
-  `codepostal` varchar(5) NOT NULL,
-  `ville` varchar(20) NOT NULL,
-  `date_abonnement` datetime DEFAULT NULL
+CREATE TABLE `chef` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(60) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activated` tinyint(4) DEFAULT '0',
+  `code_verification` varchar(20) NOT NULL,
+  `activation_expiry` datetime NOT NULL,
+  `statut` tinyint(4) DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `chef`
+--
+
+INSERT INTO `chef` (`ID`, `email`, `password`, `prenom`, `nom`, `created_at`, `updated_at`, `activated`, `code_verification`, `activation_expiry`, `statut`) VALUES
+(23, 'alexisvafiadis@gmail.com', '$2y$10$8V.exLWpJwLN8BxnSPXywOKdK2ERK/GuKQq.qrX9aQk2i3aoorFj2', 'Alexis', 'Vafiadis', '2022-12-16 14:12:36', '2022-12-16 14:12:36', 0, 'f10d62dd8bb32840', '2022-12-17 14:12:36', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `code_admin`
+--
+
+CREATE TABLE `code_admin` (
+  `code` varchar(20) NOT NULL,
+  `valide` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `code_admin`
+--
+
+INSERT INTO `code_admin` (`code`, `valide`) VALUES
+('PDQ5695F2E36Z0M0P6L2', 1);
 
 -- --------------------------------------------------------
 
@@ -72,6 +136,7 @@ CREATE TABLE `entreprise` (
 CREATE TABLE `mesure` (
   `ID` int(11) NOT NULL,
   `ID_capteur` int(11) NOT NULL,
+  `ID_utilisateur` int(11) NOT NULL,
   `DateTime` datetime NOT NULL,
   `valeur` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -83,12 +148,20 @@ CREATE TABLE `mesure` (
 --
 
 CREATE TABLE `question` (
-  `ID` bigint(20) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `ID_categorie` int(11) NOT NULL,
   `position` tinyint(4) NOT NULL,
-  `sujet` varchar(300) NOT NULL,
-  `reponse` varchar(9999) NOT NULL,
-  `ID_categorie` tinyint(4) NOT NULL
+  `sujet` varchar(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `reponse` varchar(9999) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`ID`, `ID_categorie`, `position`, `sujet`, `reponse`) VALUES
+(14, 1, 1, 'Comment se connecter Ã  son compte?', 'AprÃ¨s avoir crÃ©Ã© votre compte, cliquez sur \"sign in\" en haut Ã  droite de la page.'),
+(10, 2, 1, 'Comment gÃ©rer ses employÃ©s', 'Une rÃ©ponse');
 
 -- --------------------------------------------------------
 
@@ -97,26 +170,30 @@ CREATE TABLE `question` (
 --
 
 CREATE TABLE `utilisateur` (
-  `ID` bigint(20) UNSIGNED NOT NULL,
-  `IDEntreprise` int(10) NOT NULL,
-  `statut` tinyint(4) NOT NULL,
+  `ID` int(10) UNSIGNED NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `homme` tinyint(4) DEFAULT NULL,
+  `genre` varchar(1) DEFAULT NULL,
   `prenom` varchar(50) NOT NULL,
   `nom` varchar(60) NOT NULL,
   `telephone` varchar(10) NOT NULL,
-  `datenaissance` date DEFAULT NULL,
+  `date_naissance` date DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `activated` tinyint(4) DEFAULT NULL,
   `code_verification` varchar(15) NOT NULL,
-  `banni` tinyint(4) DEFAULT NULL
+  `statut` tinyint(4) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `capteur`
@@ -125,10 +202,22 @@ ALTER TABLE `capteur`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `entreprise`
+-- Indexes for table `categorie`
 --
-ALTER TABLE `entreprise`
+ALTER TABLE `categorie`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `chef`
+--
+ALTER TABLE `chef`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `code_admin`
+--
+ALTER TABLE `code_admin`
+  ADD PRIMARY KEY (`code`);
 
 --
 -- Indexes for table `mesure`
@@ -153,15 +242,25 @@ ALTER TABLE `utilisateur`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `capteur`
 --
 ALTER TABLE `capteur`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `entreprise`
+-- AUTO_INCREMENT for table `categorie`
 --
-ALTER TABLE `entreprise`
-  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `categorie`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `chef`
+--
+ALTER TABLE `chef`
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `mesure`
 --
@@ -171,12 +270,12 @@ ALTER TABLE `mesure`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
