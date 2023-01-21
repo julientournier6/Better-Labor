b75328e21535c3e2fac6454b440697aafa93dc31
alexisvafiadis <104735242+alexisvafiadis@users.noreply.github.com>
2023-01-21 13:15:27 +0100

    esaz
    esza

--- database/config.php
+++ database/config.php
@@ -1,14 +1,21 @@
 <?php
-
-$hostName = "localhost";
-$userName = "root";
-$pass = "";
-$databaseName = "betterlabor";
-
+$herogu = true;
+if ($herogu) {
+  $hostName = "herogu.garageisep.com";
+  $userName = "MZdD21NaxX_betterlabo";
+  $pass = "IsFwuk8VfFuUP8vx";
+  $databaseName = "P9VdwiMT4I_betterlabo";
+}
+else {
+  $hostName = "localhost";
+  $userName = "root";
+  $pass = "";
+  $databaseName = "betterlabor";
+}
 // $conn = new mysqli($hostName, $userName, $pass, $databaseName);
-$conn = new mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
-if ($conn->connect_error) {
-  die("La connexion a échoué : " . $conn->connect_error);
+$conn = new PDO("mysqlhost:host=$hostName;dbname=$databaseName", $userName, $pass);
+if ($conn->errorInfo()) {
+  die("La connexion a échoué : " . $conn->errorInfo());
 }
 function redirect($url) {
   ob_start();
