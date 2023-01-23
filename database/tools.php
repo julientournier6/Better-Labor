468a90403c3cea22a3f4b79b198364f2a68a2ac3
alexisvafiadis <104735242+alexisvafiadis@users.noreply.github.com>
2023-01-23 20:20:19 +0100

    esaz

--- database/tools.php
+++ database/tools.php
@@ -243,7 +243,7 @@ function specific_sign_in($conn, $role) {
 //On veut déterminer l'adresse de base du serveur pour pouvoir envoyer un lien d'activation
 //car l'adresse n'est pas la même pour tout le monde
 function get_base_url() {
-    $url = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];//on prend d'abord l'adresse actuelle
+    $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];//on prend d'abord l'adresse actuelle
     $url = explode("espace", $url)[0];//on enlève la partie en trop"
     return $url;
 }
